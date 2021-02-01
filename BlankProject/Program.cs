d12a9c1e64dd018c235eb810992d4d636a45c3f9
Tony Collins <tjc@eltham-college.org.uk>
2021-02-01 08:52:50 +0000

    Add project files.

---
+++ BlankProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlankProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
